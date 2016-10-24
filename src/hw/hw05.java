cc78f85e32b305e3ba1474b7424000c9566ca760
t537511634 <105021029@live.asia.edu.tw>
2016-10-24 18:28:53 +0800

    GG

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -9,18 +9,12 @@ public class hw05 {
 		System.out.println("請輸入一正整數n:");
 		int n = scn.nextInt(), a = 0, b = 0, c = 1, t = 0;
 		int k[][] = new int[n][n];
-		while (a + b <= n) {
-			for (int i = 0; i < a; i++) {
-
-				for (int j = 0; j < b; j++) {
-					k[a][b] = c;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j <= i; j++) {
+					k[i-j][j] = c;
+					c++;
 				}
-			}t++;
-			if (a + b < t) {
-				a++;
-				b++;
 			}
-		}
 		for (int i = 0; i < k.length; i++) {
 			for (int j = 0; j < k[i].length; j++) {
 				System.out.print(k[i][j] + "\t");
